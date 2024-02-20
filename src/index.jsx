ee689292106d9363bde3ad358cff8d702ccb2e51
Sergey Kabanets <skabanets97@gmail.com>
2024-02-21 00:52:02 +0200

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+import { App } from "./components";
+import "./index.css";
+import "modern-normalize/modern-normalize.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <App />
+  </>
+);
