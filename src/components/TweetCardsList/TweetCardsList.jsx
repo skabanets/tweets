c46580b94f1914d76d1bbe8ddc0255a7e70d6c58
Sergey Kabanets <skabanets97@gmail.com>
2024-02-22 07:14:14 +0200

    fix issue with rerender page

--- src/components/TweetCardsList/TweetCardsList.jsx
+++ src/components/TweetCardsList/TweetCardsList.jsx
@@ -1,32 +1,51 @@
-import { useSelector } from 'react-redux';
-import { selectIsError, selectIsLoading } from '../../redux/users/slice';
-import { Loader } from '../Loader/Loader';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectTotalUsers, setInitialValue } from '../../redux/users/slice';
 import { TweetCard } from '../TweetCard/TweetCard';
 import s from './TweetCardsList.module.css';
 import { toast } from 'react-toastify';
+import { useEffect, useState } from 'react';
+import { fetchTotalUsers, fetchUsers } from '../../redux/users/operations';
+import { LoadMoreButton } from '../LoadMoreButton/LoadMoreButton';
 
-export const TweetCardsList = ({ tweets }) => {
-  const isLoading = useSelector(selectIsLoading);
-  const isError = useSelector(selectIsError);
+export const TweetCardsList = () => {
+  const [page, setPage] = useState(1);
+  const limit = 3;
+  const totalUsers = useSelector(selectTotalUsers);
+  const totalPages = Math.ceil(totalUsers / limit);
+  const [loadedUsers, setLoadedUsers] = useState([]);
 
-  if (isLoading) {
-    return <Loader />;
-  }
+  const dispatch = useDispatch();
 
-  if (isError) {
-    toast.error('Something went wrong! Reload page or try again later.');
-  }
+  useEffect(() => {
+    if (page === 1) {
+      dispatch(setInitialValue());
+      dispatch(fetchTotalUsers());
+    }
+    dispatch(fetchUsers(page))
+      .unwrap()
+      .then(newUsers => {
+        setLoadedUsers(prevUsers => [...prevUsers, ...newUsers]);
+      })
+      .catch(error => {
+        toast.error('Something went wrong! Reload page or try again later.');
+      });
+  }, [page, dispatch]);
+
+  const handleClickLoadMoreBtn = () => {
+    setPage(prevPage => prevPage + 1);
+  };
 
   return (
     <div>
       <ul className={s.tweetsCardsList}>
-        {tweets.length !== 0 &&
-          tweets.map(user => (
+        {loadedUsers.length !== 0 &&
+          loadedUsers.map(user => (
             <li key={user.id}>
               <TweetCard user={user} />
             </li>
           ))}
       </ul>
+      {page < totalPages && <LoadMoreButton handleClickButton={handleClickLoadMoreBtn} />}
     </div>
   );
 };
--- src/pages/Tweets/Tweets.jsx
+++ src/pages/Tweets/Tweets.jsx
@@ -1,38 +1,11 @@
-import { useEffect, useState } from 'react';
 import { ButtonGoBack } from '../../components';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchTotalUsers, fetchUsers } from '../../redux/users/operations';
 import { TweetCardsList } from '../../components/TweetCardsList/TweetCardsList';
-import { selectTotalUsers, selectUsers, setInitialValue } from '../../redux/users/slice';
-import { LoadMoreButton } from '../../components/LoadMoreButton/LoadMoreButton';
 
 const Tweets = () => {
-  const [page, setPage] = useState(1);
-  const limit = 3;
-  const totalUsers = useSelector(selectTotalUsers);
-  const totalPages = Math.ceil(totalUsers / limit);
-  const tweets = useSelector(selectUsers);
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(setInitialValue());
-    dispatch(fetchTotalUsers());
-  }, [dispatch]);
-
-  useEffect(() => {
-    dispatch(fetchUsers(page));
-  }, [page, dispatch]);
-
-  const handleClickLoadMoreBtn = () => {
-    setPage(prevPage => prevPage + 1);
-  };
-
   return (
     <div className="pageWrapper">
       <ButtonGoBack />
-      <TweetCardsList tweets={tweets} />
-      {page < totalPages && <LoadMoreButton handleClickButton={handleClickLoadMoreBtn} />}
+      <TweetCardsList />
     </div>
   );
 };
